76ba00f50e2407f4a1802ea764c8b32588fbfbd5
Rick13211 <rick13211roy@gmail.com>
2025-08-03 18:04:33 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import React, { useState } from 'react'
-import ExpenseTracker from './ExpenseTracker/components/ExpenseTracker'
-import PasswordGenerator from './PasswordGenerator/PasswordGenerator'
+import React from 'react'
 
 const App = () => {
-  
-  
   return (
-    <div className="min-h-screen bg-grey-50 flex-center justify-center">
-      <PasswordGenerator/>
+    <div>
+      
     </div>
   )
 }
