0e3704629b9ce46da31ef0af162e8aade3ae551e
Rick13211 <rick13211roy@gmail.com>
2025-08-03 16:50:25 +0530

    completed expense tracker and started password generator

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
+import ExpenseTracker from './ExpenseTracker/components/ExpenseTracker'
 
 const App = () => {
   
   
   return (
     <div>
-      
+      <ExpenseTracker/>
     </div>
   )
 }
